ed7a91921d1d5e082baff084b3f4c4ea485a6d74
Ilya Kisil <ilyakisil@gmail.com>
2018-05-13 15:21:46 +0100

    fix

--- config_setup.sh
+++ config_setup.sh
@@ -9,18 +9,20 @@ rm $HOME/.gitconfig
 rm $HOME/.config/git/gitignore
 rm $HOME/.ssh/config
 
+#---------- Create directories
+mkdir $HOME/Mount
+mkdir $HOME/Mount/1-EEE-ik1614
+mkdir $HOME/.config/git
+
 #---------- Copy conf files or create symlinks
 CPY='cp -r'  # use CPY='ln -sf' for symlinks
 $CPY $PWD/bash/bashrc $HOME/.bashrc
 $CPY $PWD/bash/bash_profile $HOME/.bash_profile
 $CPY $PWD/bash/bash_aliases $HOME/.bash_aliases
-$CPY $PWD/bash/scripts $HOME/.bash_scripts
+$CPY $PWD/bash/scripts $HOME/scripts
 $CPY $PWD/git/gitconfig $HOME/.gitconfig
 $CPY $PWD/git/gitignore $HOME/.config/git/gitignore
 $CPY $PWD/ssh/config $HOME/.ssh/config
-
-#---------- Create directories
-mkdir $HOME/Mount/1-EEE-ik1614
 
 
 source ~/.bashrc
